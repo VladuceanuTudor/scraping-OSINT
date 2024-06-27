4b73d97d9bac36d9390305a055888de532e21992
vladuceanu.tudor <tudorvladuceanu@gmail.com>
2024-06-27 20:44:55 +0300

    final touch + fb validation fix

--- osint_endpoint_app.py
+++ osint_endpoint_app.py
@@ -153,7 +153,7 @@ def check_instagram_profile(username):
 def check_facebook_profile(username):
     service = Service(executable_path="./chromedriver")
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--headless')  
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--ignore-certificate-errors")
@@ -164,31 +164,46 @@ def check_facebook_profile(username):
 
     try:
         driver.get(f"https://www.facebook.com/{username}/")
-        WebDriverWait(driver, 10).until(EC.title_contains("Facebook"))
+        WebDriverWait(driver, 10).until(EC.title_contains("Facebook"))  
 
         if "Page Not Found" in driver.title:
             return False
 
         try:
+            
             accept_cookies_div = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, "//*[@id='facebook']/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[2]/div[1]"))
             )
             accept_cookies_div.click()
         except Exception as e:
+            
             return False
 
+        
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//*[@aria-label='Close']"))
         )
 
+        
         buttonX = driver.find_element(By.XPATH, "//*[@aria-label='Close']")
         buttonX.click()
 
+        time.sleep(2)
+        # driver.refresh()  
+        # elements_with_text = driver.find_elements(By.XPATH, "//*[text()]")
+
+
+        # for element in elements_with_text:
+        # print(element.text)
         return True
     except Exception as e:
+        
         return False
+
     finally:
         driver.quit()
+
+    return True
 
 def search_google(driver, query):
     search_url = f"https://www.google.com/search?q={query}"
@@ -248,13 +263,13 @@ def search_web():
         if title is None or page_content.startswith('Error'):
             results.append({
                 "url": url,
-                "error": page_content  # Include the error message in results
+                "error": page_content  
             })
         else:
             results.append({
                 "url": url,
                 "title": title,
-                "content": page_content[:200]  # Return only first 200 characters of content
+                "content": page_content[:200]  
             })
     
     driver.quit()
